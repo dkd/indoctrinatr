9cfa9e0982123befe8125d2e47e29f75323ba8e2
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-03-20 12:26:07 +0100

    fixed specs to work with Kaminari pagination

--- spec/views/document_submissions/index.html.erb_spec.rb
+++ spec/views/document_submissions/index.html.erb_spec.rb
@@ -2,20 +2,19 @@ require 'spec_helper'
 
 describe "document_submissions/index" do
   before(:each) do
-    assign(:document_submissions, [
+    assign(:document_submissions, Kaminari.paginate_array([
       stub_model(DocumentSubmission,
         template: FactoryGirl.build(:template)
       ),
       stub_model(DocumentSubmission,
         template: FactoryGirl.build(:template)
       )
-    ])
+    ]).page(1))
   end
 
   it "renders a list of document_submissions" do
-    pending
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => nil.to_s, :count => 2
+    assert_select "tr>td", :text => /Name/, :count => 2
   end
 end
--- spec/views/templates/index.html.erb_spec.rb
+++ spec/views/templates/index.html.erb_spec.rb
@@ -2,22 +2,13 @@ require 'spec_helper'
 
 describe "templates/index" do
   before(:each) do
-    assign(:templates, [
-      stub_model(Template,
-        :name => "Name",
-        :content => "MyText"
-      ),
-      stub_model(Template,
-        :name => "Name",
-        :content => "MyText"
-      )
-    ])
+    assign(:templates, Kaminari.paginate_array(create_list(:template, 2)).page(1))
   end
 
   it "renders a list of templates" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "Name".to_s, :count => 2
+    assert_select "tr>td", :text => /Name/, :count => 2
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
   end
 end
