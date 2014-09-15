06d066c3dd15170321d29ca89d8f68a60e4f1096
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-09-15 14:51:15 +0200

    made specs running

--- spec/controllers/template_packs_controller_spec.rb
+++ spec/controllers/template_packs_controller_spec.rb
@@ -1,5 +1,5 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe TemplatePacksController, :type => :controller do
-
+describe TemplatePacksController do
+  pending
 end
--- spec/helpers/template_packs_helper_spec.rb
+++ spec/helpers/template_packs_helper_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require 'spec_helper'
 
 # Specs in this file have access to a helper object that includes
 # the TemplatePackHelper. For example:
@@ -10,6 +10,6 @@ require 'rails_helper'
 #     end
 #   end
 # end
-RSpec.describe TemplatePacksHelper, :type => :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe TemplatePacksHelper do
+  pending
 end
