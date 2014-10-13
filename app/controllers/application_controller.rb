2cdd1f4656403728e159cc6321d7186fc5730141
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-10-13 08:48:54 +0200

    more I18N

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  VALID_LOCALES_AS_REGEX = /(en|de)/
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
@@ -8,10 +9,14 @@ class ApplicationController < ActionController::Base
   private
 
   def set_locale
-    I18n.locale = extract_certain_locales_from_accept_language_header || I18n.default_locale
+    I18n.locale = extract_certrain_locals_from_params || extract_certain_locales_from_accept_language_header || I18n.default_locale
+  end
+
+  def extract_certrain_locals_from_params
+    params.fetch(:locale, "")[VALID_LOCALES_AS_REGEX]
   end
 
   def extract_certain_locales_from_accept_language_header
-    request.env['HTTP_ACCEPT_LANGUAGE'][/(en|de)/]
+    request.env['HTTP_ACCEPT_LANGUAGE'][VALID_LOCALES_AS_REGEX]
   end
 end
