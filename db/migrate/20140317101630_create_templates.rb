2465e808a1eff5c7d10270955f34c42e3b0e803e
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2014-11-03 11:53:01 +0100

    fixed issues with MySQL

--- db/migrate/20140317101630_create_templates.rb
+++ db/migrate/20140317101630_create_templates.rb
@@ -2,7 +2,7 @@ class CreateTemplates < ActiveRecord::Migration
   def change
     create_table :templates do |t|
       t.string :name, default: '', null: false
-      t.text :content, default: '', null: false
+      t.text :content, null: false
 
       t.timestamps
     end
