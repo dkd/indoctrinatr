f4468dbe2784e290caede07275d4f4c92cdf9cca
Wilfried Irsslinger <wilfried.irsslinger@dkd.de>
2015-02-18 10:36:14 +0100

    Issue #58428 update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,18 +14,18 @@
 ActiveRecord::Schema.define(version: 20150212084746) do
 
   create_table "document_submissions", force: :cascade do |t|
-    t.integer  "template_id", null: false
+    t.integer  "template_id",              null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.text     "content"
+    t.text     "content",     default: "", null: false
   end
 
   add_index "document_submissions", ["template_id"], name: "index_document_submissions_on_template_id"
 
   create_table "submitted_template_fields", force: :cascade do |t|
-    t.integer  "document_submission_id",   null: false
-    t.integer  "template_field_id",        null: false
-    t.text     "value",                    null: false
+    t.integer  "document_submission_id",                            null: false
+    t.integer  "template_field_id",                                 null: false
+    t.string   "value",                    limit: 255, default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "file_upload_file_name"
@@ -39,7 +39,7 @@ ActiveRecord::Schema.define(version: 20150212084746) do
 
   create_table "template_fields", force: :cascade do |t|
     t.string   "name",              limit: 255, default: "",     null: false
-    t.text     "default_value",                                  null: false
+    t.string   "default_value",     limit: 255, default: "",     null: false
     t.string   "presentation",      limit: 255, default: "text", null: false
     t.integer  "template_id",                                    null: false
     t.datetime "created_at"
@@ -71,7 +71,5 @@ ActiveRecord::Schema.define(version: 20150212084746) do
     t.integer  "template_pack_id"
     t.string   "output_file_name",    limit: 255
   end
-
-  add_index "templates", ["template_pack_id"], name: "index_templates_on_template_pack_id"
 
 end
