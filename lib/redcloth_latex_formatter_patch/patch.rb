f81d146c1c893fefb979e4271900e311e00e1adc
Nicolai Reuschling <nicolai.reuschling@dkd.de>
2015-08-24 10:19:01 +0200

    Redmine Issue #64623: patch RedCloth to generate appropriate tables

--- lib/redcloth_latex_formatter_patch/patch.rb
+++ lib/redcloth_latex_formatter_patch/patch.rb
@@ -1,20 +1,35 @@
-module RedCloth::Formatters::LATEX
-  { :h1 => 'chapter',
-    :h2 => 'section',
-    :h3 => 'subsection',
-    :h4 => 'subsubsection'
-  }.each do |m,tag| 
-    define_method(m) do |opts| 
-      case opts[:align]
-      when 'left' then
-        "\\begin{flushleft}\\#{tag}{#{opts[:text]}}\\end{flushleft}\n\n" 
-      when 'right' then
-        "\\begin{flushright}\\#{tag}{#{opts[:text]}}\\end{flushright}\n\n" 
-      when 'center' then
-        "\\begin{center}\\#{tag}{#{opts[:text]}}\\end{center}\n\n" 
-      else
-        "\\#{tag}{#{opts[:text]}}\n\n"
+require 'redcloth'
+
+module RedCloth
+  module Formatters
+    module LATEX
+      { :h1 => 'chapter',
+        :h2 => 'section',
+        :h3 => 'subsection',
+        :h4 => 'subsubsection'
+      }.each do |m,tag|
+        define_method(m) do |opts|
+          case opts[:align]
+          when 'left' then
+            "\\begin{flushleft}\\#{tag}{#{opts[:text]}}\\end{flushleft}\n\n"
+          when 'right' then
+            "\\begin{flushright}\\#{tag}{#{opts[:text]}}\\end{flushright}\n\n"
+          when 'center' then
+            "\\begin{center}\\#{tag}{#{opts[:text]}}\\end{center}\n\n"
+          else
+            "\\#{tag}{#{opts[:text]}}\n\n"
+          end
+        end
       end
-    end 
-  end 
+
+      def table_close(opts)
+        output  = "\\begin{tabu}{ #{"l " * @table[0].size }}\n"
+        @table.each do |row|
+          output << "    #{row.join(" & ")} \\\\\n"
+        end
+        output << "\\end{tabu}\n"
+        output
+      end
+    end
+  end
 end
